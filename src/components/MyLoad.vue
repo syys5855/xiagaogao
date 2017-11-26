728e1437649064e5a2fe0a996c64bc6763b00844
守护mine <mine>
2017-11-26 19:37:08 +0800

    load finish

--- src/components/MyLoad.vue
+++ src/components/MyLoad.vue
@@ -1,106 +1,104 @@
 <template>
-<div class="load">
-    <div class="load-wrap">
-      <div class="load-circlr-wrap left">
+  <div class="load">
+       <div class="load-cover left">
         <div class="load-circle"></div>
       </div>
-      <div class="load-circlr-wrap right">
+      <div class="load-cover right">
         <div class="load-circle"></div>
       </div>
-    </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .load {
-  animation: loadMove 1332ms linear infinite;
-  width: 80px;
-  height: 80px;
-}
-.load-wrap {
   position: relative;
-  width: 100%;
-  height: 100%;
-  animation: wrapMove 2664ms linear infinite;
-  /* animation: wrapMove 2000ms  cubic-bezier(.4,0,.2,1) infinite; */
+  width: 100px;
+  height: 100px;
+  animation: loadMove 2000ms linear infinite;
+  &-circle {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 200%;
+    height: 100%;
+    border: 3px solid   red;
+    border-radius: 50%;
+  }
+
+  &-basic {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    .load-circle {
+      width: 100%;
+      border-color: red;
+      border-width: 4px;
+    }
+  }
+
+  &-cover {
+    position: absolute;
+    top: 0;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+
+    &.right {
+      right: 0;
+      transform-origin: left center;
+      .load-circle {
+        left: -100%;
+        border-left-color: transparent;
+        border-bottom-color: transparent;
+        transform: rotateZ(-135deg);
+        animation: rightMove 1500ms linear  infinite;
+      }
+    }
+    &.left {
+      left: 0;
+      transform-origin: right center;
+      .load-circle {
+        border-right-color: transparent; border-bottom-color: transparent;
+        transform: rotateZ(135deg);
+        animation: leftMove 1500ms linear infinite;
+      }
+    }
+  }
 }
 
-.load-circlr-wrap {
-  position: absolute;
-  width: 50%;
-  height: 100%;
-  overflow: hidden;
+@keyframes loadMove {
+  from{
+    transform: rotateZ(0deg);
+  }
+  to{
+    transform: rotateZ(360deg);
+  }
 }
-.load-circlr-wrap.left {
-  left: 0;
-  top: 0;
-}
-
-.load-circlr-wrap.left .load-circle {
-  transform: rotateZ(39deg);
-  animation: leftMove 666ms cubic-bezier(0.4, 0, 0.2, 1) infinite alternate both;
-}
-
-.load-circlr-wrap.right {
-  right: 0;
-  top: 0;
-  transform-origin: left center;
-}
-.load-circlr-wrap.right .load-circle {
-  left: -100%;
-  transform: rotateZ(-129deg);
-
-  animation: rightMove 666ms cubic-bezier(0.4, 0, 0.2, 1) infinite alternate both;
-}
-
-.load-circle {
-  position: absolute;
-  width: 200%;
-  height: 100%;
-  border: 3px solid red;
-  border-radius: 50%;
-  border-bottom-color: transparent;
-  border-left-color: transparent;
+@keyframes leftMove {
+  0%,25%{
+    transform: rotateZ(135deg);
+  }
+  75%,100%{
+    transform: rotateZ(495deg);
+  }
 }
 
 @keyframes rightMove {
   0% {
-    transform: rotateZ(-129deg);
+    transform: rotateZ(-135deg);
   }
-  100% {
-    transform: rotateZ(0deg);
-  }
-}
-
-@keyframes leftMove {
-  0% {
-    transform: rotateZ(39deg);
-  }
-  100% {
-    transform: rotateZ(-90deg);
+  25%,37.5%{
+    transform: rotateZ(45deg);
   }
 
-}
-
-@keyframes wrapMove {
-  0% {
-    transform: rotateZ(0deg);
-  }
-
-  100% {
-    transform: rotateZ(360deg);
-  }
-}
-@keyframes loadMove {
-  from {
-    transform: rotateZ(0deg);
-  }
-  to {
-    transform: rotateZ(360deg);
+  50%,100%{
+    transform: rotateZ(225deg);
   }
 }
 </style>
